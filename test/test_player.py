77bfcfb2669205fb363213663caf63bebce15c46
jheddings <jason@heddings.com>
2019-01-23 22:13:56 -0700

    Test refactoring

--- test/test_player.py
+++ test/test_player.py
@@ -6,7 +6,7 @@ import unittest
 import idlerpg
 
 # keep logging output to a minumim for testing
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.FATAL)
 
 ################################################################################
 class IdleRPGPlayerTest(unittest.TestCase):
@@ -15,30 +15,25 @@ class IdleRPGPlayerTest(unittest.TestCase):
     def test_BasicLoadFromXML(self):
         player = idlerpg.Player()
 
-        self.assertTrue(player.load_from_file("test/jarvis_online.xml"))
+        self.assertTrue(player.load_from_file('test/jarvis_31_online.xml'))
         self.assertEqual(player.username, 'jarvis')
 
     #---------------------------------------------------------------------------
     def test_LoadBadFormatXML(self):
         player = idlerpg.Player()
 
-        self.assertFalse(player.load_from_file("test/bad_format.xml"))
-        self.assertFalse(player.load_from_file("test/bad_schema.xml"))
+        self.assertFalse(player.load_from_file('test/bad_format.xml'))
+        self.assertFalse(player.load_from_file('test/bad_schema.xml'))
 
     #---------------------------------------------------------------------------
-    def test_CheckOnlineOffline(self):
+    def test_BasicPlayerInfo(self):
         player = idlerpg.Player()
 
-        player.load_from_file("test/jarvis_online.xml")
+        player.load_from_file('test/jarvis_31_online.xml')
         self.assertTrue(player.isOnline())
+        self.assertEqual(player.level, 31)
 
-        player.load_from_file("test/jarvis_offline.xml")
+        player.load_from_file('test/jarvis_31_offline.xml')
         self.assertFalse(player.isOnline())
-
-    #---------------------------------------------------------------------------
-    def test_CheckPlayerLevel(self):
-        player = idlerpg.Player()
-
-        player.load_from_file("test/jarvis_online.xml")
         self.assertEqual(player.level, 31)
 
