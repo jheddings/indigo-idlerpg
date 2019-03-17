684835f5505a942820a5c8da2e5a10cf1b04f8b3
jheddings <jason@heddings.com>
2019-03-16 21:08:23 -0600

    Added iterator and MODE command; minor refactoring

--- src/irc.py
+++ src/irc.py
@@ -9,6 +9,8 @@
 import socket
 import logging
 
+# XXX should commands like JOIN and PART wait until the server acks before returning?
+
 ################################################################################
 class Client:
 
@@ -25,6 +27,11 @@ class Client:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.nick = nick
         self.name = name
+
+    #---------------------------------------------------------------------------
+    # iterate over server responses - this iterator will block until data is read
+    def __iter__(self):
+        return self
 
     #---------------------------------------------------------------------------
     # send one line of text
@@ -63,8 +70,9 @@ class Client:
         return text
 
     #---------------------------------------------------------------------------
-    def _next_reply(self):
-        line = self.keep_alive()
+    # processes the next server response and only returns the reply code
+    def _next_code(self):
+        line = self.next()
 
         if (line is not None and line.startswith(':')):
             return line.split(' ', 2)[1]
@@ -95,6 +103,10 @@ class Client:
         self._send('PRIVMSG %s :%s' % (recip, msg))
 
     #---------------------------------------------------------------------------
+    def mode(self, nick, flags):
+        self._send('MODE %s %s' % (nick, flags))
+
+    #---------------------------------------------------------------------------
     def quit(self, msg=None):
         if (msg is None):
             self._send('QUIT')
@@ -111,7 +123,7 @@ class Client:
 
         # read until RPL_WELCOME (001)
         while (not reply == '001'):
-            reply = self._next_reply()
+            reply = self._next_code()
 
         # XXX the only problem with clearing all banner messages is that the
         # client could end up blocking for a very long time if the server just
@@ -121,20 +133,20 @@ class Client:
         # read until end of welcome messages...
         if (clearWelcomeMessages):
             while (reply.startswith('0')):
-                reply = self._next_reply()
+                reply = self._next_code()
 
         # what about MOTD (376) or no MOTD (422) ?
 
     #---------------------------------------------------------------------------
     # this method is blocking and should usually be called on a separate thread
-    def keep_alive(self):
-        text = self._recv()
+    def next(self):
+        reply = self._recv()
 
         # TODO are there other responses we should handle automatically?
 
-        if (text is not None and text.startswith('PING')):
-            msg = text.split(':', 1)[1]
+        if (reply is not None and reply.startswith('PING')):
+            msg = reply.split(':', 1)[1]
             self._send('PONG :%s' % msg)
 
-        return text
+        return reply
 
